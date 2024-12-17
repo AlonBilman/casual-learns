223868649530192633ba8a55e1f3bc52469864f4
Alon Bilman <bilman.alon@gmail.com>
2024-12-17 19:20:37 +0200

    set3

--- set_3.hs
+++ set_3.hs
@@ -10,5 +10,15 @@ shiftString x = let y = last x : x in init y
     one of them is a result of applying shiftString 
     one or more times -}
 
---isShifted :: String -> String -> Bool 
+isShifted :: String -> String -> Bool 
+isShifted "" "" = True
+isShifted x y = (length x == length y)  && 
+                    foldl(\acc index -> acc || shiftStringFor index x == y) False (take (length x) [1..])
+                    where 
+                        shiftStringFor :: Int -> String -> String 
+                        shiftStringFor _ "" = ""
+                        shiftStringFor 0 x = x
+                        shiftStringFor i x = foldl(\acc _-> shiftString acc) x (take i [1..])
 
+                        
+----------------------------------------THIRD QUESTION-----------------------------------------------
