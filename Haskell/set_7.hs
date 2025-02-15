c58eee44a65b819a889cbb223f92f1c35a894eac
Alon Bilman <bilman.alon@gmail.com>
2025-02-15 15:45:22 +0200

    set_7

--- Haskell/set_7.hs
+++ Haskell/set_7.hs
@@ -44,9 +44,9 @@ For example, toString (BinOp (Value2) Sub (Value 3)) should return “2-3”.
 
 {-Answer-}
 
-toString :: Expr -> String
-toString (Value x) = show x 
-toString (BinOp ex1 op ex2) = toString ex1 ++ realOp ++ toString ex2
+expToString :: Expr -> String
+expToString (Value x) = show x 
+expToString (BinOp ex1 op ex2) = expToString ex1 ++ realOp ++ expToString ex2
                             where 
                                 realOp
                                     | op == Add = "+"
@@ -108,3 +108,79 @@ mapMatrix2 func mat = map (\(row,rowInx) -> (map (\(col, colInx) -> func col (ro
 
 transposeMatrix :: Matrix t -> Matrix t -- ofc it has to be squared.. for non - see set_4
 transposeMatrix mat = mapMatrix2 (\_ (row,col) -> (mat !! col) !! row ) mat 
+
+
+
+--From 2024 - hw2
+
+data BinTree = Empty | Leaf Int | Node BinTree Int BinTree deriving (Show,Eq)
+
+{-
+The first data type defines a simple tree containing Int values. Such a tree can be represented as a string. 
+For example:   
+    Node (Leaf 4) 3 (Node Empty 3 (Node (Leaf 1) 2 (Leaf 2))) can be represented as "3(4,3(,2(1,2)))" 
+Important:  
+    1. We use only positive integers in this question 
+    2. The string representation might contain blanks that do not alter its value. For example “1( , 2)“ 
+       is the same as “1(,2)”.
+-}
+
+{-
+2.a 
+    Write a function to_string, which takes a BinTree and returns its string representation 
+    as described above. 
+-}
+
+treeToString :: BinTree -> String
+treeToString Empty = "";
+treeToString (Leaf x) = show x 
+treeToString (Node tree1 val tree2) = show val ++ "(" ++ treeToString tree1 ++","++ treeToString tree2 ++ ")"  
+
+{-
+2.b
+Write a function tokenize, which takes a string representing a BinTree and returns a Maybe list of 
+its tokens. 
+The possible tokens are taken from the following type definition. 
+The function should return Nothing if the given string cannot be tokenized. 
+Examples :  
+tokenize "1(,3(,))" should return Just [Val 1,LPar,Comma,Val 3,LPar,Comma,RPar,RPar] 
+tokenize "1(,3(,x))" should return Nothing 
+-}
+
+data BinTreeTok = LPar | RPar | Comma  | Val Int deriving (Show,Eq)
+
+tokenize :: String -> Maybe [BinTreeTok]
+tokenize str = let y = filter(/= ' ') str in
+    if foldl (\acc x -> acc && (x `elem` "0123456789(),")) True y then Just (tokenizeHelper y) else Nothing
+    where 
+        tokenizeHelper :: String -> [BinTreeTok]
+        tokenizeHelper str = reverse(foldl(\acc x -> convert x : acc) [] str)
+        convert :: Char -> BinTreeTok
+        convert x 
+                |x == '(' = LPar
+                |x == ')' = RPar
+                |x == ',' = Comma
+                |otherwise = Val (read [x] :: Int)
+
+{-
+2.c 
+Write a function compile, which takes a valid representation string and returns the BinTree it 
+represents.
+-}
+
+compile :: String -> BinTree
+compile s = fst (parseTree (fromJust (tokenize s))) 
+    where 
+        fromJust (Just x) = x
+
+        parseTree :: [BinTreeTok] -> (BinTree, [BinTreeTok])
+        parseTree (Val n : LPar : ts) =
+            let (left,  Comma : ts2) = parseMaybeTree ts    --rec
+                (right, RPar  : ts3) = parseMaybeTree ts2   --rec
+            in (Node left n right, ts3) 
+        parseTree (Val n : ts) = (Leaf n, ts)               --leaf
+
+        parseMaybeTree :: [BinTreeTok] -> (BinTree, [BinTreeTok])
+        parseMaybeTree (Comma : ts) = (Empty, Comma : ts)           --"(_,"
+        parseMaybeTree (RPar  : ts) = (Empty, RPar  : ts)           --",_)"  
+        parseMaybeTree ts = parseTree ts                            --regular 
